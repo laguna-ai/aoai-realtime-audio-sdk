b1ca1a91dab027763e584b3d317a11b7860038cb
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2025-07-11 16:39:08 -0500

    redeploy

--- src/app/chat-interface.tsx
+++ src/app/chat-interface.tsx
@@ -46,7 +46,7 @@ const ChatInterface = () => {
   const endpoint = process.env.NEXT_PUBLIC_AZURE_OPENAI_ENDPOINT || "";
   const deployment = process.env.NEXT_PUBLIC_AZURE_OPENAI_DEPLOYMENT || "";
   console.log("deployment:", deployment);
-  // Lo anterior se deja para que el usuario pueda cambiarlo si es necesario
+  
   const [useVAD, setUseVAD] = useState(true);
   const [instructions, setInstructions] = useState("");
   const [temperature, setTemperature] = useState(0.9);
